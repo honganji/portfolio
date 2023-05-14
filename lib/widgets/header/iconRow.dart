b5ea5e38f6355a63100f52a5e85559eb6123fe02
github honganji <yujicavs@icloud.com>
2023-05-14 10:50:07 +0900

    add icon row to header

--- lib/widgets/header/iconRow.dart
+++ lib/widgets/header/iconRow.dart
@@ -8,9 +8,38 @@ class IconRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      color: Colors.pink,
-      child: const Text("Icon Row part"),
+      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+      child: Row(
+        children: [
+          const Icon(Icons.abc),
+          const Spacer(),
+          Row(
+            children: [
+              Image.asset(
+                "assets/img/github_icon.png",
+                height: 25,
+                width: 25,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Image.asset(
+                "assets/img/linkedin_icon.png",
+                height: 25,
+                width: 25,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Image.asset(
+                "assets/img/twitter_icon.png",
+                height: 25,
+                width: 25,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
