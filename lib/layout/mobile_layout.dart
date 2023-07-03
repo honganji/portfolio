611c456d0844d0db2582ec542da30633e9fb9959
github honganji <yujicavs@icloud.com>
2023-07-03 16:13:32 +0900

    setup appber of mobile front end

--- lib/layout/mobile_layout.dart
+++ lib/layout/mobile_layout.dart
@@ -1,10 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/sections/about/mobile_about.dart';
+import 'package:portfolio/sections/experience/mobile_experience.dart';
+import 'package:portfolio/sections/home/mobile_home.dart';
+import 'package:portfolio/sections/projects/mobile_projects.dart';
+import 'package:portfolio/sections/skills/mobile_skills.dart';
+import 'package:portfolio/widgets/mobile_list_tile.dart';
 
 class MobileLayout extends StatelessWidget {
   const MobileLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Mobile");
+    List<Widget> widgetList = [
+      const SafeArea(child: MobileHome()),
+      const SafeArea(child: MobileAbout()),
+      const SafeArea(child: MobileExperience()),
+      const SafeArea(child: MobileSkills()),
+      const SafeArea(child: MobileProjects()),
+    ];
+    PageController controller = PageController();
+    return Scaffold(
+      endDrawer: Drawer(
+        backgroundColor: Colors.black,
+        child: ListView(
+          children: [
+            MobileListTile(
+              title: "Home",
+              controller: controller,
+              icon: Icons.home,
+              pageNum: 1,
+            ),
+            MobileListTile(
+              title: "About",
+              controller: controller,
+              icon: Icons.person,
+              pageNum: 2,
+            ),
+            MobileListTile(
+              title: "Experience",
+              controller: controller,
+              icon: Icons.work,
+              pageNum: 3,
+            ),
+            MobileListTile(
+              title: "Skills",
+              controller: controller,
+              icon: Icons.palette_outlined,
+              pageNum: 4,
+            ),
+            MobileListTile(
+              title: "Projects",
+              controller: controller,
+              icon: Icons.build,
+              pageNum: 5,
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.asset(
+              "assets/img/Y_icon.png",
+              filterQuality: FilterQuality.medium,
+              fit: BoxFit.fill,
+              height: 40,
+              width: 40,
+            ),
+            const SizedBox(
+              width: 3,
+            ),
+            const Text(
+              "Yuji Toshihiro",
+              style: TextStyle(fontSize: 30, fontFamily: "Vollkorn"),
+            ),
+          ],
+        ),
+      ),
+      body: PageView(
+        scrollDirection: Axis.vertical,
+        pageSnapping: false,
+        controller: controller,
+        children: widgetList,
+      ),
+    );
   }
 }
