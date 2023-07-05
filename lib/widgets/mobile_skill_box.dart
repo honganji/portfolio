c11a525a89806a3d0c7611d7604aaf78b3d4866f
github honganji <yujicavs@icloud.com>
2023-07-05 14:40:22 +0900

    modify mobile UI of experience section

--- lib/widgets/mobile_skill_box.dart
+++ lib/widgets/mobile_skill_box.dart
@@ -10,61 +10,37 @@ class MobileSkillBox extends StatefulWidget {
 }
 
 class _DesktopSkillBoxState extends State<MobileSkillBox> {
-  bool isHover = false;
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      opaque: false,
-      onHover: (s) {
-        setState(() {
-          isHover = true;
-        });
-      },
-      onExit: (s) {
-        setState(() {
-          isHover = false;
-        });
-      },
-      child: Container(
-        margin: const EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: const Color.fromARGB(255, 50, 50, 50),
-          boxShadow: [
-            BoxShadow(
-              color: isHover
-                  ? const Color.fromARGB(255, 125, 61, 0)
-                  : Colors.black,
-              offset: isHover ? const Offset(2.0, 2.0) : const Offset(0, 0),
-              blurRadius: 4.0,
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: const Color.fromARGB(255, 165, 165, 165),
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: width * 0.007,
+          ),
+          Text(
+            '${widget.skill.framworkName}\n(${widget.skill.languageName})',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 13,
             ),
-          ],
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: width * 0.007,
-            ),
-            Text(
-              '${widget.skill.framworkName}\n(${widget.skill.languageName})',
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 13,
-              ),
-            ),
-            const Spacer(),
-            Image.asset(
-              widget.skill.imagePath,
-              filterQuality: FilterQuality.medium,
-              fit: BoxFit.fill,
-              width: width * 0.07,
-              // height: height * 0.1,
-            ),
-            const Spacer(),
-          ],
-        ),
+          ),
+          const Spacer(),
+          Image.asset(
+            widget.skill.imagePath,
+            filterQuality: FilterQuality.medium,
+            fit: BoxFit.fill,
+            width: width * 0.07,
+            // height: height * 0.1,
+          ),
+          const Spacer(),
+        ],
       ),
     );
   }
