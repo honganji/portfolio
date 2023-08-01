579dbe8ed082b3fca804cf6158f38def8974cab6
github honganji <yujicavs@icloud.com>
2023-08-01 07:23:32 +0300

    modify link

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:portfolio/router.dart';
 import 'package:portfolio/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:url_strategy/url_strategy.dart';
 import 'firebase_options.dart';
 
 void main() async {
+	setPathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
