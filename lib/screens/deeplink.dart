d7ee50aa0b893ae40ae115360ec0fcf9b5335810
github honganji <yujicavs@icloud.com>
2023-08-01 06:52:27 +0300

    add go_router for deeplink

---
+++ lib/screens/deeplink.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class DeepLink extends StatelessWidget {
+  const DeepLink({super.key, required this.userId, required this.path});
+  final String userId;
+  final String path;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("deeplink page"),
+      ),
+      body: Center(child: Column(
+        children: [
+          const Text("You reached to deeplink page!"),
+          Text("Deeplink URL: \nhttps://$userId.com/$path/")
+        ],
+      )),
+    );
+  }
+}
