d78f3b63bbe652aa9adc9b4a05faea2edbfa63fb
github honganji <yujicavs@icloud.com>
2023-08-17 10:23:23 +0900

    deal with wild card

---
+++ lib/screens/deeplink2_2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class DeepLink2_2 extends StatelessWidget {
+  const DeepLink2_2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("You reached to deeplink page 2-1!")),
+    );
+  }
+}
